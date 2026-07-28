4908d5ac56676792155b66b0e2db82a0fc94b2d0
Farhad Kamal <jitubd1971@gmail.com>
2026-07-28 23:41:51 +0600

    Resume menu music on the game-over screen
    Battle music previously just stopped at game-over, leaving it silent
    after the victory fanfare. Now the menu track picks back up, staying
    seamless into a New Match since it's already playing by then.

--- js/ui/dashboardScreen.js
+++ js/ui/dashboardScreen.js
@@ -9,7 +9,7 @@ import { renderLogPanel } from './logPanel.js';
 import { showModal } from './modal.js';
 import { renderRulesModal } from './rulesScreen.js';
 import { rollChaosGamble } from '../engine/random.js';
-import { playActionSound, playUiClick, playKO, playVictory, playCoin, playSound, startTickLoop, stopTickLoop, stopBattleMusic } from '../engine/sound.js';
+import { playActionSound, playUiClick, playKO, playVictory, playCoin, playSound, startTickLoop, stopTickLoop, startMenuMusic } from '../engine/sound.js';
 
 const COIN_FLIP_ACTIONS = new Set(['cyclonePunch']);
 const CHAOS_GAMBLE_ACTIONS = new Set(['chaosGamble']);
@@ -175,8 +175,8 @@ export function renderDashboard(container, game, { onRestart }) {
       clearTurnTimer();
       if (!victorySoundPlayed) {
         victorySoundPlayed = true;
-        stopBattleMusic();
         if (game.winnerPlayerId) playVictory();
+        startMenuMusic();
       }
       container.appendChild(renderGameOver());
       return;
